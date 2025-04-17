72b559b173f2e1c31edf159b2bca67ac41cf1768
Rishav Kant <rishav.khg@gmail.com>
2025-04-17 13:59:38 -0500

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -465,7 +465,7 @@ query = st.text_area('', height=250)
 query = (query + " Show the output in strctured format using tables")
 
 
-if query:
+if len(query)>3:
     st.chat_message("user").write(query)
     with st.chat_message("assistant"):
         response = agent.run(query)
